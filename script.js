15ec4d73f4e029c190c6bdcc8d49457fb0ad0957
agapitoo <fernandaagapito2013@gmail.com>
2023-01-09 22:27:19 -0300

    :tada: commit inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode(){
+  const html = document.documentElement
+   html.classList.toggle("light")
+  //if(html.classList.contains('light')){
+  //  html.classList.remove('light')
+ // } else{
+  //  html.classList.add('light')
+ // }
+//se - senão ,  faça
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")){
+    img.setAttribute("src", "./assets/Avatar2.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+}
